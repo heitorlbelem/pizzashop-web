83b850df6dc39e67eec8ffb16936b560bcacd5d9
Heitor Belém <heitorlbelem98@gmail.com>
2024-10-08 20:38:49 -0300

    Creates mock for order-details request

---
+++ src/api/mocks/get-order-details-mock.ts
@@ -0,0 +1,38 @@
+import { http, HttpResponse } from 'msw'
+
+import {
+  GetOrderDetailsParams,
+  GetOrderDetailsResponse,
+} from '../get-order-details'
+
+export const getOrderDetailsMock = http.get<
+  GetOrderDetailsParams,
+  never,
+  GetOrderDetailsResponse
+>('/orders/:orderId', ({ params }) => {
+  return HttpResponse.json({
+    id: params.orderId,
+    createdAt: new Date().toISOString(),
+    status: 'pending',
+    totalInCents: 5000,
+    customer: {
+      email: 'john.doe@example.com',
+      name: 'John Doe',
+      phone: '61981818282',
+    },
+    orderItems: [
+      {
+        id: 'order-item-1',
+        priceInCents: 1000,
+        product: { name: 'Pizza Pepperoni' },
+        quantity: 1,
+      },
+      {
+        id: 'order-item-2',
+        priceInCents: 2000,
+        product: { name: 'Pizza Calabresa' },
+        quantity: 2,
+      },
+    ],
+  })
+})
